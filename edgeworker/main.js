e3aa543f1d867437b673e3de80fcef503bdd40b0
bailey <bailey@harperdb.io>
2025-08-11 08:56:38 -0600

    rm caching from ew

--- edgeworker/main.js
+++ edgeworker/main.js
@@ -1,11 +1,9 @@
 import { httpRequest } from 'http-request';
 import { logger } from 'log';
-import { createCache } from 'cache';
 
 const HARPER_TOKEN = '';
 const HARPER_BASE_URL = '';
 const PMUSER_103_HINTS = 'PMUSER_103_HINTS';
-const cache = createCache('early-hints-cache');
 
 const OPTIONS = {
 	method: 'GET',
@@ -20,23 +18,14 @@ export async function onClientRequest(request) {
 	if (!isMainDocumentNavigation(request)) return;
 
 	try {
-		const cacheKey = `${request.scheme}://${request.host}${request.url}`;
-		let serialized = await cache.get(cacheKey);
-
-		if (!serialized) {
-			const encodedPageUrl = encodeURIComponent(cacheKey);
-			const url = `${HARPER_BASE_URL}/${encodedPageUrl}?select(serializedLinks)`;
-			const response = await httpRequest(url, OPTIONS);
-			if (response.status === 200) {
-				serialized = await response.json();
-				if (serialized && typeof serialized === 'string') {
-					await cache.set(cacheKey, serialized, { ttl: 300 }); // cache for 5 minutes
-				}
+		const encodedPageUrl = encodeURIComponent(`${request.scheme}://${request.host}${request.url}`);
+		const url = `${HARPER_BASE_URL}/${encodedPageUrl}?select(serializedLinks)`;
+		const response = await httpRequest(url, OPTIONS);
+		if (response.status === 200) {
+			const serialized = await response.json();
+			if (serialized && typeof serialized === 'string') {
+				request.setVariable(PMUSER_103_HINTS, serialized);
 			}
-		}
-
-		if (serialized && typeof serialized === 'string') {
-			request.setVariable(PMUSER_103_HINTS, serialized);
 		}
 	} catch (exception) {
 		logger.log(`Error occured while calling HDB: ${exception.message}`);
