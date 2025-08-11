35a643cb3c9b4df79c74db24974e0139a7b5d2c1
deliciousmonster <jaxon@deliciousmonster.com>
2025-08-11 13:10:19 -0400

    Refine EW

--- edgeworker/main.js
+++ edgeworker/main.js
@@ -1,30 +1,30 @@
 import { httpRequest } from 'http-request';
 import { logger } from 'log';
 
-const HARPER_TOKEN = '';
-const HARPER_BASE_URL = '';
-const PMUSER_103_HINTS = 'PMUSER_103_HINTS';
+const HARPER_INSTANCE_APPLICATION_URL = '[YOUR_HARPER_INSTANCE_APPLICATION_URL]';
+const HARPER_INSTANCE_TOKEN = '[YOUR_BASE64_ENCODED_HARPER_USER:PASS]';
+const ORIGIN_SITE_BASE_URL = "www.harpersystems.dev";
 
 const OPTIONS = {
 	method: 'GET',
 	headers: {
-		'Authorization': `Basic ${HARPER_TOKEN}`,
+		'Authorization': `Basic ${HARPER_INSTANCE_TOKEN}`,
 		'Content-Type': 'application/json',
 	},
 };
 
 export async function onClientRequest(request) {
 	try {
-		const encodedPageUrl = encodeURIComponent(`${request.scheme}://${request.host}${request.url}`);
+		const encodedPageUrl = encodeURIComponent(`${request.scheme}://${ORIGIN_SITE_BASE_URL}${request.url}`);
 
-		const url = `${HARPER_BASE_URL}/${encodedPageUrl}?select(serializedLinks)`;
+		const url = `https://${HARPER_INSTANCE_APPLICATION_URL}/hints?q=${encodedPageUrl}`;
 
 		const response = await httpRequest(url, OPTIONS);
 
 		if (response.status === 200) {
 			const serialized = await response.json();
 			if (serialized && typeof serialized === 'string') {
-				request.setVariable(PMUSER_103_HINTS, serialized);
+				request.setVariable('PMUSER_103_HINTS', serialized);
 			}
 		}
 	} catch (exception) {
