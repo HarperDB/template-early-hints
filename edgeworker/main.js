b462fca81d677764c497efc083afa2a769d0ac12
bailey dunning <67710155+baileydunning@users.noreply.github.com>
2025-09-03 15:03:51 -0600

    feat: safari fallback support (#3)

--- edgeworker/main.js
+++ edgeworker/main.js
@@ -3,7 +3,10 @@ import { logger } from 'log';
 
 const HARPER_INSTANCE_APPLICATION_URL = '[YOUR_HARPER_INSTANCE_APPLICATION_URL]';
 const HARPER_INSTANCE_TOKEN = '[YOUR_BASE64_ENCODED_HARPER_USER:PASS]';
-const ORIGIN_SITE_BASE_URL = 'www.harpersystems.dev';
+
+const staticPreconnectHosts = [
+	'https://fonts.googleapis.com',
+];
 
 const OPTIONS = {
 	method: 'GET',
@@ -11,20 +14,49 @@ const OPTIONS = {
 		'Authorization': `Basic ${HARPER_INSTANCE_TOKEN}`,
 		'Content-Type': 'application/json',
 	},
-	timeout: 50,
+	timeout: 250,
 };
+
+function isSafari(userAgent) {
+	userAgent = String(userAgent).toLowerCase();
+
+	const hasSafari = userAgent.includes('safari');
+	const hasWebKit = userAgent.includes('applewebkit');
+	const hasVersionTag = userAgent.includes('version/'); // Safari-specific token
+
+	// Exclude other brands (desktop & iOS shells)
+	const otherBrands = /(crios|chrome\/|chromium|edg\/|edgios|fxios|opr\/|opios|samsungbrowser|yabrowser|ucbrowser|brave|vivaldi|electron|duckduckgo)/;
+	const isOther = otherBrands.test(userAgent);
+
+	return hasWebKit && hasSafari && hasVersionTag && !isOther;
+}
 
 export async function onClientRequest(request) {
 	const secFetchMode = request.getHeader('sec-fetch-mode');
-    const hasNavigate = Array.isArray(secFetchMode)
-        ? secFetchMode.includes('navigate')
-        : secFetchMode === 'navigate';
+	const hasNavigate = Array.isArray(secFetchMode)
+		? secFetchMode.includes('navigate')
+		: secFetchMode === 'navigate';
 	if (!hasNavigate) {
-	    return;
+		return;
 	}
 
 	try {
-		const encodedPageUrl = encodeURIComponent(`${request.scheme}://${ORIGIN_SITE_BASE_URL}${request.url}`);
+		const encodedPageUrl = encodeURIComponent(`${request.scheme}://${request.host}${request.url}`);
+		const userAgent = request.getHeader('User-Agent');
+
+		// For Safari, add static preconnect headers directly since Safari doesn’t support Early Hints. This reduces latency and ensures external resources are preconnected.
+		const safariHints = [];
+		if (isSafari(userAgent)) {
+			staticPreconnectHosts.forEach(host => {
+				safariHints.push(`<${host}>;rel=preconnect;crossorigin`);
+			});
+		}
+
+		if (safariHints.length > 0) {
+			const hintString = safariHints.join(',');
+			request.setVariable('PMUSER_103_HINTS', hintString);
+			return;
+		}
 
 		const url = `https://${HARPER_INSTANCE_APPLICATION_URL}/hints?q=${encodedPageUrl}`;
 
